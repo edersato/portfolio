34a6d762a618502556140d0c5744893e450e9f70
Eder Sato <64936033+edersato@users.noreply.github.com>
2020-08-30 20:39:23 -0300

    Pasta JS

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function dropdown() {
+    var mostra = document.getElementById("menu-mobile");
+
+        if (mostra.style.display === "block") {
+            mostra.style.display = "none";
+        } else {
+            mostra.style.display = "block";
+        }
+  }
